1259314ea72ea20f6cfe7a55fb33714c8a197aee
isel-jao <iseljao@pop-os.localdomain>
2021-09-24 22:49:13 +0100

    array

---
+++ regex.js
@@ -0,0 +1,22 @@
+"use strict"
+
+
+////////////////////////////////////// Flags //////////////////////////////////////
+
+// --> i <--	case-insensitive
+// --> g <--	all matches (without it – only the first match is returned)
+// --> m <--	Multiline mode ??
+// --> s <--	dotall” mode ??
+// --> u <--	Enables full Unicode support ??
+// --> y <--	??
+
+
+
+////////////////////////////////////// Symbols //////////////////////////////////////
+// $&			inserts the match
+// $`			before the match
+// $'			after the match
+// $$			inserts character $
+// $n			if n is a 1-2 digit number, then it inserts the contents of n-th parentheses ??
+// $<name>		inserts the contents of the parentheses with the given name, ??
+
